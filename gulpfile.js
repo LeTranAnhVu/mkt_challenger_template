495fb6b25bd394b6e748023b67122b42162f9222
hieutranminh <hieu.tranminhhcm@gmail.com>
2019-09-23 21:11:56 +0700

    Done news and remove import font awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var webpack = require('webpack');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass',function(){
@@ -15,24 +14,9 @@ gulp.task('watch', function() {
       baseDir: "./src"
     }
   });
-  gulp.watch(['src/assets/js/modules/**/*.js'], ['waitForScripts']);
   gulp.watch('src/*.html',browserSync.reload);
   gulp.watch('src/*/*.html',browserSync.reload);
   gulp.watch('src/assets/scss/**/*.scss', ['sass']);
   gulp.watch('src/assets/css/**/*.css',browserSync.reload);
 });
 
-gulp.task('scripts', function(callback) {
-  webpack(require('./webpack.config.js'), function(err, stats) {
-    if (err) {
-      console.log(err.toString());
-    }
-
-    console.log(stats.toString());
-    callback();
-  });
-});
-
-gulp.task('waitForScripts', ['scripts'], function() {
-  browserSync.reload();
-});
